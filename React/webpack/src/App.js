9849b5c925df775b83f226c2f23f698f89706f0e
workmadmax <80357524+workmadmax@users.noreply.github.com>
2021-09-22 14:48:16 -0300

    Webpack
    configurando webpack - "pausa para almoço"

---
+++ React/webpack/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button";
+
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Work MadMax 42</p>
+      <p>Webpack 4</p>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
---
+++ React/webpack/src/Button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Button = () => <button>MADMAX 42</button>;
+
+export default Button;
